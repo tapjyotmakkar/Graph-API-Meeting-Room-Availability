73fca0c3bc950bd0fcd4c466b6b7dce33714006c
tapjyotmakkar <tapjyotmakkar@gmail.com>
2021-07-01 21:21:11 +1000

    Initial upload

---
+++ TestGraphApi/GraphConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace TestGraphApi
+{
+    public class GraphConfig
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+    }
+}
